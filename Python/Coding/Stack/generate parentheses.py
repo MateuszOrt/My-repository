642cd08b3e1ff2756cdbbf65d651be0f090a1b58
MateuszOrt <orteqq@gmail.com>
2023-12-05 12:27:27 +0100

    Update generate parentheses.py

--- Python/Coding/Stack/generate parentheses.py
+++ Python/Coding/Stack/generate parentheses.py
@@ -29,3 +29,29 @@ class Solution(object):
 
 s=Solution()
 s.generateParenthesis(2)               
+#%%
+class Solution(object):
+    def generateParenthesis(self, n):
+            
+            res = []
+            
+            def backtrack(open_n, closed_n, path):
+                
+    
+                if open_n == closed_n == n:
+                    res.append(path)
+                    return
+                
+    
+                if open_n < n:
+                    backtrack(open_n + 1, closed_n, path + "(")
+                 
+    
+                if closed_n < open_n:
+                    backtrack(open_n, closed_n + 1, path + ")")
+                    
+            backtrack(0, 0, "")
+            return res
+        
+s=Solution()
+s.generateParenthesis(2)
